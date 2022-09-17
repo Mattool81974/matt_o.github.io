f4a6c0b11697aa85610eae8f26f70d6662cc975d
Mattool81974 <mmattmmo@gmail.com>
2022-09-17 23:12:32 +0200

    Site web 1-1

---
+++ programme.js
@@ -0,0 +1,5 @@
+let boutonHome = document.getElementById('home');
+let boutonPresentation = document.getElementById('presentation');
+let boutonYoutube = document.getElementById('youtube');
+let boutonProjet = document.getElementById('projet');
+
