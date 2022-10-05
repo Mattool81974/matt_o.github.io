c968095cce8301f8f98884b8a39dfea68cec18a1
Mattool81974 <mmattmmo@gmail.com>
2022-10-05 21:58:11 +0200

    Site web 3-1-2

--- other/news/newsapi.js
+++ other/news/newsapi.js
@@ -8,6 +8,8 @@ async function getNews(){
     let NewsParse = Parse.Object.extend("News");
     //Query pour rechercher tout les éléments dans "News"
     let query = new Parse.Query(NewsParse);
+    //Texte finale pour les news
+    let texteFinale = "";
     try{
         //Obtenir tout les éléments dans "News"
         const news = await query.find();
@@ -17,6 +19,36 @@ async function getNews(){
             news.removeItem(0, 1);
         }
         
+        //Créer le html de news conteneur
+        for(let i = 0;i<4;i++)
+        {
+            let date = "";
+            if(news[i].get("Date").getDate() < 10)
+            {
+                date += "0" + String(news[i].get("Date").getDate());
+            }
+            else
+            {
+                date += String(news[i].get("Date").getDate());
+            }
+            date += "/";
+            if(news[i].get("Date").getMonth() + 1 < 10)
+            {
+                date += "0" + String(news[i].get("Date").getMonth() + 1);
+            }
+            else
+            {
+                date += String(news[i].get("Date").getMonth() + 1);
+            }
+            date += "/" + String(news[i].get("Date").getFullYear());
+            texteFinale += "<article class=\"news\" style=\"background-image:url('" + news[i].get("ImageLien") + "');" + news[i].get("StyleNews") + "\">";
+            texteFinale += "<h1 style=\"" + news[i].get("StyleNewsH1") + "\">" + news[i].get("Titre") + "</h1>";
+            texteFinale += "<div class=\"support\" style=\"" + news[i].get("StyleNewsTexte") + "\"><p>" + news[i].get("Texte") + "</br></p></div>";
+            texteFinale += "<h2 style=\"" + news[i].get("StyleNewsH2") + "\">Ajouté par " + news[i].get("Auteur") + " le " + date + "</h2>";
+            texteFinale += "</article>";
+        }
+
+        document.getElementById('newsconteneur').innerHTML = texteFinale;
     }
     catch
     {
