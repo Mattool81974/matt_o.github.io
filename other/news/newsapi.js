04ecc28b60e3e31ba9dc62fdc9b3aa4011983b17
Mattool81974 <mmattmmo@gmail.com>
2022-10-10 21:50:26 +0200

    Site web 3-2

--- other/news/newsapi.js
+++ other/news/newsapi.js
@@ -8,8 +8,10 @@ async function getNews(){
     let NewsParse = Parse.Object.extend("News");
     //Query pour rechercher tout les éléments dans "News"
     let query = new Parse.Query(NewsParse);
-    //Texte finale pour les news
-    let texteFinale = "";
+    //Texte finale pour les news (configuré pour une erreur)
+    let texteFinale = "Erreur de connection inconnu.";
+    //Style du div parent (pour erreur de connection)
+    let styleParent = "display: inline-flex;flex-wrap: wrap;";
     try{
         //Obtenir tout les éléments dans "News"
         const news = await query.find();
@@ -19,8 +21,10 @@ async function getNews(){
             news.removeItem(0, 1);
         }
         
+        //Reset du texte finale (pas d'erreur de connection)
+        texteFinale = "";
         //Créer le html de news conteneur
-        for(let i = 0;i<4;i++)
+        for(let i = 0;i<news.length;i++)
         {
             let date = "";
             if(news[i].get("Date").getDate() < 10)
@@ -47,13 +51,18 @@ async function getNews(){
             texteFinale += "<h2 style=\"" + news[i].get("StyleNewsH2") + "\">Ajouté par " + news[i].get("Auteur") + " le " + date + "</h2>";
             texteFinale += "</article>";
         }
-
-        document.getElementById('newsconteneur').innerHTML = texteFinale;
     }
     catch
     {
         //Erreur lors de la recherche (pas de co, serveur down...)
+        styleParent = "display: block;";
+        texteFinale = "<p style=\"text-align: center;align-self: center;font-size: 23px;\">Erreur de connection...</p>"
     }
+
+    //Assigner le style parent
+    document.getElementById('newsconteneur').setAttribute('style', styleParent);
+    //Assigner le texte
+    document.getElementById('newsconteneur').innerHTML = texteFinale;
 }
 
 window.addEventListener('load', getNews());
