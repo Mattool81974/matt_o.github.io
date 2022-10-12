d6b1a94c5e071ebb5786ab50c3a68becf99031a8
= <mmattmmo@gmail.com>
2022-10-12 11:51:10 +0200

    Site web 3-3

---
+++ other/projects/projectsapi.js
@@ -0,0 +1,14 @@
+//connexion aux bases de données Parse
+Parse.initialize("AMa09UbnCvFl8yWQNkDaNRaaEWstk33D96fDHWm5", "jy5DhPcKZqCzQN0HaB8u7fYlHQC5g2OHVxV4WVYs");
+Parse.serverURL = "https://parseapi.back4app.com/";
+
+async function loadProject(p){
+    alert("Pas encore mais tkt");
+}
+
+//Fonction asynchrone pour charges les projets
+async function getProjects(){
+    
+}
+
+window.addEventListener('load', getProjects());
